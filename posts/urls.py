c4bc1fee645ff3139bc70aa8cfd381bfa3a02412
Nimeshrxi <nimeshraivibes@gmail.com>
2026-09-12 16:29:52 +0530

    Added interactivity, stylesheet, forms, buttons, django's built-in authentication url

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.post_list, name="post_list"),
+    path("create/", views.post_create, name="post_create"),
     path("<int:post_id>/", views.post_details, name="post_detail"), # type: ignore
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from posts.models import Post
-from django.shortcuts import get_object_or_404
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,3 +13,18 @@ def post_list(request):
 def post_details(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, "posts/post_detail.html", {"post": post})
+
+@login_required
+
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            form.save()
+            return redirect("post_list")
+    else:
+        form = PostForm()
+            
+    return  render(request, "posts/post_create.html", {"form": form})
