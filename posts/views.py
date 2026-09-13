8c163891355aed5cc3479b3084b4be2e23bd7d78
Nimeshrxi <nimeshraivibes@gmail.com>
2026-09-13 18:15:33 +0530

    Added the delete and comment and page functionalities

--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from posts.models import Post
-from .forms import PostForm
+from posts.models import Post, Comment
+from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, "posts/post_list.html", {"posts": posts})
+    posts = Post.objects.all().order_by("-created_at")
+    
+    paginator = Paginator(posts, 5)
+    
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, "posts/post_list.html", {"page_obj": page_obj})
 
 def post_details(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-    return render(request, "posts/post_detail.html", {"post": post})
+    
+    if request.method == "POST":
+        if not request.user.is_authenticated:
+            return redirect("login")
+        
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect("post_detail", post_id = post.id) #type: ignore
+    else:
+        form = CommentForm()
+    return render(request, "posts/post_detail.html", {"post": post, "form": form,})
 
 @login_required
 
@@ -65,3 +86,14 @@ def register(request):
         form = UserCreationForm()
         
     return render(request, "registration/register.html", {"form": form})
+
+@login_required
+def comment_delete(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+    
+    if request.method == "POST":
+        post_id = comment.post.id
+        comment.delete()
+        return redirect("post_detail", post_id=post_id)
+    
+    return render(request, "posts/comment_delete.html", {"comment": comment})
